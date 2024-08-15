db34214f9958adb42293841d745ae784d26a97b6
Jaisung Lee <jaisung.lee@fteam.kr>
2024-08-15 18:16:08 +0900

    차트: 예시 CSV 데이터 및 파싱 추가

--- app/c001/[code]/C001.tsx
+++ app/c001/[code]/C001.tsx
@@ -4,24 +4,29 @@ import { useRef } from "react";
 import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 import type { Chart as ChartJS } from "chart.js";
+import type { MarcapData } from "@/utils/fetch";
 
-export default function C001() {
+export default function C001({ data }: { readonly data: MarcapData[] }) {
   const chartRef = useRef<ChartJS>(null);
 
   const chartType = "line"; // https://www.chartjs.org/docs/latest/charts/line.html
 
-  const chartData = {
-    labels: [1, 2, 3, 4, 5, 6, 7],
-    datasets: [
-      {
-        label: "Dataset#1",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.1,
-      },
-    ],
-  };
+  let chartData;
+  if (data != null && data.length > 0) {
+    const [firstData] = data;
+
+    chartData = {
+      datasets: [
+        {
+          label: `${firstData.Name}(${firstData.Code})`,
+          data: data.map(({ Date: d, Close }) => ({ x: d, y: Close })),
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+        },
+      ],
+    };
+  }
 
   return <Chart ref={chartRef} type={chartType} data={chartData} />;
 }
--- app/c001/[code]/page.tsx
+++ app/c001/[code]/page.tsx
@@ -1,14 +1,18 @@
 import C001 from "./C001";
+import { getMarcapData } from "@/utils/fetch";
 
-export default function Page({
+export default async function Page({
   params,
 }: {
   readonly params: { readonly code: string };
 }) {
+  const { code } = params;
+  const data = await getMarcapData(code);
+
   return (
     <>
-      <div>code: {params.code}</div>
-      <C001></C001>
+      <div>Code: {code}</div>
+      <C001 data={data}></C001>
     </>
   );
 }
