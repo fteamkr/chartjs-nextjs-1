2fc39f05fc716a11a20f0279ee0322604d36b798
Jaisung Lee <jaisung.lee@fteam.kr>
2024-08-15 18:38:28 +0900

    차트: 일자/시간 관련 패키지 추가 및 적용

--- app/c001/[code]/C001.tsx
+++ app/c001/[code]/C001.tsx
@@ -3,6 +3,8 @@
 import { useRef } from "react";
 import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
+import dayjs from "@/utils/dayjs";
+import "chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm"; // https://github.com/bolstycjw/chartjs-adapter-dayjs-4
 import type { Chart as ChartJS } from "chart.js";
 import type { MarcapData } from "@/utils/fetch";
 
@@ -12,21 +14,56 @@ export default function C001({ data }: { readonly data: MarcapData[] }) {
   const chartType = "line"; // https://www.chartjs.org/docs/latest/charts/line.html
 
   let chartData;
+  let chartOptions;
   if (data != null && data.length > 0) {
     const [firstData] = data;
 
     chartData = {
       datasets: [
+        // https://www.chartjs.org/docs/latest/charts/line.html#dataset-properties
         {
           label: `${firstData.Name}(${firstData.Code})`,
-          data: data.map(({ Date: d, Close }) => ({ x: d, y: Close })),
+          data: data.map(({ Date: d, Close: c }) => ({
+            x: dayjs(d, "YYYY-MM-DD").valueOf(), // https://day.js.org/docs/en/parse/string-format
+            y: Number.parseInt(c, 10),
+          })),
+          parsing: false,
           fill: false,
           borderColor: "rgb(75, 192, 192)",
           tension: 0.1,
         },
       ],
     };
+
+    chartOptions = {
+      scales: {
+        // https://www.chartjs.org/docs/latest/axes/
+        x: {
+          // https://www.chartjs.org/docs/latest/axes/cartesian/time.html
+          type: "time",
+          time: {
+            unit: "day",
+            displayFormats: {
+              // https://www.chartjs.org/docs/latest/axes/cartesian/time.html#display-formats
+              day: "ll", // https://day.js.org/docs/en/display/format#list-of-localized-formats
+            },
+          },
+        },
+        y: {
+          // https://www.chartjs.org/docs/latest/axes/cartesian/linear.html
+          type: "linear",
+          beginAtZero: true,
+        },
+      },
+    };
   }
 
-  return <Chart ref={chartRef} type={chartType} data={chartData} />;
+  return (
+    <Chart
+      ref={chartRef}
+      type={chartType}
+      data={chartData}
+      options={chartOptions}
+    />
+  );
 }
